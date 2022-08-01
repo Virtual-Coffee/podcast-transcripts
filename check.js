da7073987e1fc7b363b9465b9ab6c6f5e205e889
Dan Ott <dan@dtott.com>
2022-08-01 16:01:26 -0400

    format srt chunks to 32 chars/line and 2 lines per chunk

--- check.js
+++ check.js
@@ -1,13 +1,36 @@
 var fs = require('fs');
+const path = require('path');
 
 const yargs = require('yargs');
 
+function padZeros(t, z = 2) {
+  if (z === 2) {
+    return ('00' + t).slice(-2);
+  }
+  return ('000' + t).slice(-3);
+}
+
+function srtTimeStampFromMs(ms) {
+  const hours = Math.floor(ms / 1000 / 60 / 60);
+  ms = ms - hours * 1000 * 60 * 60;
+  const minutes = Math.floor(ms / 1000 / 60);
+  ms = ms - minutes * 1000 * 60;
+  const seconds = Math.floor(ms / 1000);
+  ms = ms - seconds * 1000;
+  return `${padZeros(hours)}:${padZeros(minutes)}:${padZeros(
+    seconds
+  )},${padZeros(ms, 3)}`;
+}
+
 function checkFile(file) {
   console.log(`checking ${file}`);
+  const output = [];
   var array = fs
     .readFileSync(`${__dirname}/episodes/${file}`)
     .toString()
     .split(/\r?\n\r?\n/);
+
+  let addedLines = 0;
 
   for (let i = 0; i < array.length; i++) {
     const element = array[i];
@@ -15,11 +38,7 @@ function checkFile(file) {
 
     const ts = lines[1];
 
-    if (parseInt(lines[0]) !== i + 1) {
-      console.warn(`Index out of order: ${lines[0]} - fixing`);
-    }
-
-    lines[0] = i + 1;
+    lines[0] = i + 1 + addedLines;
 
     if (
       !/^[0-9][0-9]:[0-9][0-9]:[0-9][0-9],[0-9][0-9][0-9] --> [0-9][0-9]:[0-9][0-9]:[0-9][0-9],[0-9][0-9][0-9]$/.test(
@@ -28,10 +47,71 @@ function checkFile(file) {
     ) {
       console.error(`Incorrect timestamp formatting: ${ts}`);
     }
-    array[i] = lines.join('\n');
+
+    const [start, end] = ts.split(' --> ').map((t) => {
+      const [hours, minutes, seconds, ms] = t.split(/:|,/);
+      return (
+        parseInt(hours) * 60 * 60 * 1000 +
+        parseInt(minutes) * 60 * 1000 +
+        parseInt(seconds) * 1000 +
+        parseInt(ms)
+      );
+    });
+
+    if (start > end) {
+      console.error(`Start after end: ${ts}`);
+      console.log(start, end);
+    }
+
+    const text = lines.slice(2).join(' ').split(' ');
+    const textLines = [];
+    const maxLength = 32;
+    let curIndex = -1;
+    text.forEach((word) => {
+      if (
+        curIndex >= 0 &&
+        `${textLines[curIndex]} ${word}`.length <= maxLength
+      ) {
+        textLines[curIndex] = `${textLines[curIndex]} ${word}`;
+      } else {
+        curIndex++;
+        textLines[curIndex] = word;
+      }
+    });
+
+    if (textLines.length > 2) {
+      const numChunks = Math.ceil(textLines.length / 2);
+      const eachChunkLength = Math.floor((end - start) / numChunks);
+      let curStart = start;
+      let baseIndex = lines[0];
+      for (let i = 0; i < numChunks; i++) {
+        if (i !== 0) {
+          addedLines++;
+        }
+
+        output.push(
+          [
+            baseIndex + i,
+            `${srtTimeStampFromMs(curStart)} --> ${srtTimeStampFromMs(
+              curStart + eachChunkLength
+            )}`,
+            ...textLines.slice(i * 2, i * 2 + 2),
+          ].join('\n')
+        );
+        curStart = curStart + eachChunkLength;
+      }
+    } else {
+      output.push(
+        [
+          lines[0],
+          `${srtTimeStampFromMs(start)} --> ${srtTimeStampFromMs(end)}`,
+          ...textLines,
+        ].join('\n')
+      );
+    }
   }
 
-  fs.writeFileSync(`${__dirname}/episodes/${file}`, array.join('\n\n'));
+  fs.writeFileSync(`${__dirname}/episodes/${file}`, output.join('\n\n'));
 }
 
 const argv = yargs
@@ -53,7 +133,7 @@ if (argv.file && fs.existsSync(`${__dirname}/episodes/${argv.file}`)) {
   });
 
   files.map((file) => {
-    if (file.isFile()) {
+    if (file.isFile() && path.extname(file) === '.srt') {
       checkFile(file.name);
     }
   });
